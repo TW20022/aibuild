9670a658cbe4db32b10a8c9b10f7d974f1a77a25
TW20022 <118551729+TW20022@users.noreply.github.com>
2026-03-08 21:28:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# 移除旧的源（如果有），确保干净
+# 移除旧源
 sed -i '/printing_packages/d' feeds.conf.default
-
-# 添加前人指路的打印机驱动精准源
+# 添加打印机包（此仓库已支持 24.xx 版本）
 echo 'src-git printing_packages https://github.com/woniuzfb/openwrt-24-printing-packages' >> feeds.conf.default
