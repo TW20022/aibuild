99062609bdc338f2043460c2022d42b3b21cbdde
TW20022 <118551729+TW20022@users.noreply.github.com>
2026-03-08 18:41:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-# 修改默认IP和主机名
+# 1. 修正 IP 和主机名
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/JDCloud-Printer/g' package/base-files/files/bin/config_generate
 
-# 强行修改该机型的编译镜像限制 (从默认可能只有32MB扩充)
-# 注意：这需要根据源码中的路径调整，以下是针对大多数LEDE源码的改法
-sed -i 's/IMAGE_SIZE := .*/IMAGE_SIZE := 250000k/g' target/linux/ramips/image/mt7621.mk
+# 2. 针对 32MB 闪存的精确分区 (匹配你 mtd3 的 firmware 大小)
+# 设置内核分区为 4MB (给驱动留余量)
+sed -i 's/CONFIG_TARGET_KERNEL_PARTSIZE=.*$/CONFIG_TARGET_KERNEL_PARTSIZE=4/' .config
+# 设置 Rootfs 为 22MB (保守估计，确保 sysupgrade 不报错)
+sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=22/' .config
 
-# 设置 Rootfs 大小
-sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=160/' .config
-
-# 无线设置
+# 3. 无线配置 (CMCC-5G / 1234567890)
 sed -i 's/ssid=OpenWrt/ssid=CMCC-5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# 开启加密和密码
 sed -i '/set wireless.default_radio${i}.encryption=none/d' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/set wireless.default_radio${i}.ssid=CMCC-5G/a\                set wireless.default_radio${i}.encryption=psk2\n                set wireless.default_radio${i}.key=1234567890' package/kernel/mac80211/files/lib/wifi/mac80211.sh
