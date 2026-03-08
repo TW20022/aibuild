ff842a8f000046ebaf596f3524404061118a0e66
TW20022 <118551729+TW20022@users.noreply.github.com>
2026-03-08 22:08:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# 移除旧源
+# 移除旧的打印机包定义
 sed -i '/printing_packages/d' feeds.conf.default
-# 添加打印机包（此仓库已支持 24.xx 版本）
-echo 'src-git printing_packages https://github.com/woniuzfb/openwrt-24-printing-packages' >> feeds.conf.default
+
+# 【核心改动】直接把源码克隆到 package 目录下，跳过 feed 系统
+# 这样无论 feeds 怎么更新，编译时都会在本地 package 目录找到这些包
+git clone --depth 1 https://github.com/woniuzfb/openwrt-24-printing-packages package/printing_packages
