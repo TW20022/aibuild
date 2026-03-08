9701151162949f85111f467d2ef72b1e09b58926
TW20022 <118551729+TW20022@users.noreply.github.com>
2026-03-08 18:14:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
-# 修改默认 IP
+# 修改默认IP和主机名
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/JDCloud-Printer/g' package/base-files/files/bin/config_generate
 
-# 修改默认主题为 bootstrap (虽然默认通常是它，但强制指定一次)
-sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile
+# 强行修改该机型的编译镜像限制 (从默认可能只有32MB扩充)
+# 注意：这需要根据源码中的路径调整，以下是针对大多数LEDE源码的改法
+sed -i 's/IMAGE_SIZE := .*/IMAGE_SIZE := 250000k/g' target/linux/ramips/image/mt7621.mk
 
-# 修改主机名
-sed -i 's/OpenWrt/JDC/g' package/base-files/files/bin/config_generate
-
-# 针对 256MB NAND 闪存的分区调整
-# 内核分区给 16MB 足够了
-sed -i 's/CONFIG_TARGET_KERNEL_PARTSIZE=.*$/CONFIG_TARGET_KERNEL_PARTSIZE=16/' .config
-# 根文件系统分区给 160MB (剩下的空间预留给 UBI 损耗和管理)
+# 设置 Rootfs 大小
 sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=160/' .config
 
-# --- 无线配置自定义 ---
-# 1. 修改默认无线名称为 CMCC-5G
+# 无线设置
 sed -i 's/ssid=OpenWrt/ssid=CMCC-5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# 2. 设置无线密码为 1234567890 (并强制开启 WPA2 加密)
+# 开启加密和密码
 sed -i '/set wireless.default_radio${i}.encryption=none/d' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/set wireless.default_radio${i}.ssid=CMCC-5G/a\                set wireless.default_radio${i}.encryption=psk2\n                set wireless.default_radio${i}.key=1234567890' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# 针对可能存在的闭源驱动额外处理
-find package/ -name "mt7615.lua" | xargs sed -i 's/ssid = "OpenWrt"/ssid = "CMCC-5G"/g' 2>/dev/null
-find package/ -name "mt7615.lua" | xargs sed -i 's/encryption = "none"/encryption = "psk2"/g' 2>/dev/null
-find package/ -name "mt7615.lua" | xargs sed -i '/encryption = "psk2"/a\                key = "1234567890"' 2>/dev/null
