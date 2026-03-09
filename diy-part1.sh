824ea8bddbc4802a316527013684912a60753c14
TW20022 <118551729+TW20022@users.noreply.github.com>
2026-03-09 23:24:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# 1. 优先使用自定义打印包仓库（放在 feeds.conf 第一行）
+# 增加第三方打印包仓库，并置于首位
 sed -i '1i\src-git printing_packages https://github.com/woniuzfb/openwrt-24-printing-packages.git' feeds.conf.default
-
-# 2. 【关键】遵循指南，移除 pkg-config 对宿主机库的保留，防止交叉编译污染
-# 这能解决 python3-dbus 链接到 x86_64 库的问题
+# 隔离系统 pkg-config 污染
 sed -i '/--keep-system-cflags\|--keep-system-libs/d' tools/pkgconf/files/pkg-config
